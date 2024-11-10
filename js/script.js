8481275167a441c8fa1724b672df85bb5029233a
Satyaki Pal <satyakipal111@gmail.com>
2024-11-10 21:01:57 +0530

    black and white theme

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//  typing animation
+
+var typed = new Typed('.typing',{
+  strings:['','Web Developer','JavaScript Developer','Web Designer'],
+  typeSpeed:100,
+  BackSpeed:200,
+  loop:true
+});
