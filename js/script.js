de28a8668167874e2d2129508c51059a89086137
Satyaki Pal <satyakipal111@gmail.com>
2024-11-17 21:26:22 +0530

    adding details

--- js/script.js
+++ js/script.js
@@ -98,3 +98,26 @@ function asideSectionTogglerBtn(){
     allSection[i].classList.toggle('open');
   }
 }
+
+function getAge(dateString) {
+  var today = new Date();
+  var birthDate = new Date(dateString);
+  var age = today.getFullYear() - birthDate.getFullYear();
+  var m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+  }
+  return age;
+}
+
+document.querySelector('.about .about-content .personal-info .info-item .age span').textContent =`${getAge('1999-12-28')}`;
+
+function emailMe(){
+  const email = "satyakipal111@gmail.com";
+    const emailSubject = "Hello, Satyaki!";
+    const emailBody = "I would like to discuss something important.";
+
+    const emailLink = document.getElementById('email-link');
+    emailLink.href = `mailto:${email}?subject=${encodeURIComponent(emailSubject)}&body=${encodeURIComponent(emailBody)}`;
+}
+emailMe();
